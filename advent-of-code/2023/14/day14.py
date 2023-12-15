36dfe6ceb639cae70185a944f381bdfcc72fca28
Jhaendar <lycaon.anderson@gmail.com>
2023-12-15 21:14:26 +0800

    added partial solution to part 2 day14

--- advent-of-code/2023/14/day14.py
+++ advent-of-code/2023/14/day14.py
@@ -1,64 +1,105 @@
 # Advent of Code
 # Day 14: Parabolic Reflector Dish
 
+from collections import deque
+from typing import Generator
+
 Grid = list[list[str]]
 
 
 def parse_file(filename: str) -> Grid:
     # make a transposed grid
     grid: Grid = list()
-    transposed_grid: Grid = list()
     with open(filename, "r") as file:
         for line in file:
             line = line.strip()
-            if len(grid) == 0:
-                grid = [list() for x in range(len(line))]
-
-            for x, e in enumerate(line):
-                grid[x].append(e)
-
-        for row in grid:
-            transposed_grid.append(row)
-
-    return transposed_grid
-
-
-def move_rocks_to_left(grid: Grid):
-    for line in grid:
-        free_spot: set[int] = set()
-
-        for x, tile in enumerate(line):
-            if tile == ".":
-                free_spot.add(x)
-
-            if tile == "#":
-                free_spot = set()
-
-            if tile == "O" and free_spot:
-                current = min(free_spot)
-                free_spot.remove(current)
-
-                line[x], line[current] = line[current], line[x]
-                free_spot.add(x)
+            grid.append(list(line))
 
     return grid
 
 
+def get_ranges(
+    direction: str, height: int, width: int
+) -> Generator[tuple[int, int], None, None]:
+    if direction == "east":
+        i_range = range(height)
+        j_range = range(width)
+    elif direction == "west":
+        i_range = range(height - 1, -1, -1)
+        j_range = range(width - 1, -1, -1)
+    elif direction == "south":
+        i_range = range(height)
+        j_range = range(width)
+    elif direction == "north":
+        i_range = range(height - 1, -1, -1)
+        j_range = range(width - 1, -1, -1)
+    else:
+        raise KeyError("INVALID DIRECTION")
+
+    for i in i_range:
+        for j in j_range:
+            if direction in {"east", "west"}:
+                yield i, j
+            elif direction in {"north", "south"}:
+                yield j, i
+
+
+def move_rocks(grid: Grid, direction: str):
+    max_y = len(grid)
+    max_x = len(grid[0])
+    i_range, j_range = get_ranges(direction, max_y, max_x)
+    print(i_range, j_range)
+    for i in i_range:
+        free_spot: deque[int] = deque()
+        for j in j_range:
+            tile = grid[i][j]
+
+            if tile == ".":
+                free_spot.append(j)
+            if tile == "#":
+                free_spot = deque()
+
+            if tile == "O" and free_spot:
+                current = free_spot.popleft()
+                free_spot.append(j)
+
+                grid[i][j], grid[i][current] = grid[i][current], grid[i][j]
+
+    return grid
+
+
+def test(direction, y, x):
+    for i, j in get_ranges(direction, y, x):
+        print(i, j)
+
+
+# def spin_cycle(grid: Grid, cycles: int):
+#     cycle_direction = ""
+
+
 def calculate_total_load(grid: Grid) -> int:
     total = 0
+    max_y = len(grid)
     max_x = len(grid[0])
-    for y, line in enumerate(grid):
-        for x, tile in enumerate(line):
-            if tile == "O":
-                total += max_x - x
+    i_range, j_range = get_ranges("north", max_y, max_x)
+
+    for i in i_range:
+        for j in j_range:
+            if grid[i][j] == "O":
+                total += max_y - j
 
     return total
 
 
+def print_grid(grid: Grid):
+    for line in grid:
+        print("".join(line))
+
+
 def part_one_solution(input_file: str):
     grid = parse_file(input_file)
-    grid = move_rocks_to_left(grid)
-
+    grid = move_rocks(grid, "north")
+    print_grid(grid)
     total = calculate_total_load(grid)
     print(total)
 
@@ -66,10 +107,12 @@ def part_one_solution(input_file: str):
 if __name__ == "__main__":
     input_files: list[str] = list()
     input_files.append("./advent-of-code/2023/14/day14_input_example")
-    input_files.append("./advent-of-code/2023/14/day14_input")
+    # input_files.append("./advent-of-code/2023/14/day14_input")
 
     # Answers:
 
     for input_file in input_files:
         print(f"\nUsing input file: {input_file}")
-        part_one_solution(input_file)
+        test("north", 4, 3)
+
+        # part_one_solution(input_file)
